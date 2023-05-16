386cd17f8d6d998fe1e984538e459da2c821bfaa
Liviu Damian <liviudm@liviudm.com>
2023-05-17 02:10:29 +0300

    [lvim] add telescope fzf

--- .config/lvim/lua/plugins/telescope.lua
+++ .config/lvim/lua/plugins/telescope.lua
@@ -4,6 +4,7 @@ return {
   dependencies = {
     "nvim-lua/plenary.nvim",
     "ahmedkhalf/project.nvim",
+    { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
   },
   config = function()
     local actions = require("telescope.actions")
@@ -93,8 +94,20 @@ return {
         --   extension_config_key = value,
         -- }
         -- please take a look at the readme of the extension you want to configure
+        require('telescope').setup {
+          extensions = {
+            fzf = {
+              fuzzy = true,           -- false will only do exact matching
+              override_generic_sorter = true, -- override the generic sorter
+              override_file_sorter = true, -- override the file sorter
+              case_mode = "smart_case", -- or "ignore_case" or "respect_case"
+              -- the default case_mode is "smart_case"
+            }
+          }
+        }
       },
     })
     require("telescope").load_extension("projects")
+    require('telescope').load_extension('fzf')
   end,
 }
